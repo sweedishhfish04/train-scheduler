c80e444131bb34cb62886d81333883701a24e797
sweedishhfish04 <godziwa4@yahoo.com>
2018-06-24 21:24:46 -0500

    did almost all the coding

---
+++ app.js
@@ -0,0 +1,50 @@
+var config = {
+    apiKey: "AIzaSyDjzNlRLQcMj93B_UXbSKYvfFa5Fta9zXU",
+    authDomain: "train-scheduler-fc482.firebaseapp.com",
+    databaseURL: "https://train-scheduler-fc482.firebaseio.com",
+    projectId: "train-scheduler-fc482",
+    storageBucket: "",
+    messagingSenderId: "1091626654616"
+};
+firebase.initializeApp(config);
+
+
+function addTrainToDatabase(name, destination, time, frequency) {
+    firebase.database().ref('train/' + name.replace(/ /g,'')).set({
+        trainName: name,
+        destination: destination,
+        initialTime: time,
+        frequency: frequency
+    })
+}
+
+function getDatabaseRecords() {
+    firebase.database().ref('train').once('value').then(function(snapshot) {
+        snapshot.forEach(function(entry) {
+            console.log(entry.val())
+        })
+    })
+}
+
+
+$("#submit").click(function(){
+var name = $("#name").val()
+var destination = $("#destination").val()
+var first = $("#first").val()
+var min = $("#min").val()
+
+addTrainToDatabase(name, destination, first, min)
+
+
+console.log(name,destination,first,min)
+})
+
+
+
+getDatabaseRecords()
+
+console.log(moment().format('LT'))
+
+//addTrainToDatabase("MN_Karma_Train", "Miramar", "Now", 25)
+//var hoursAndMinutes = first.split(':')
+//first = new Date(Date.UTC(0, 0, 0, hoursAndMinutes[0], hoursAndMinutes[1]))
