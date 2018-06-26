0c311c8f1f98c08dc5254859214a18f8d8ea6337
sweedishhfish04 <godziwa4@yahoo.com>
2018-06-25 20:59:15 -0500

    Finished up the JS

--- app.js
+++ app.js
@@ -8,12 +8,16 @@ var config = {
 };
 firebase.initializeApp(config);
 
-
 function addTrainToDatabase(name, destination, time, frequency) {
+    var hoursAndMinutes = time.split(':')
+    var date = new Date()
+    date.setUTCHours(hoursAndMinutes[0])
+    date.setUTCMinutes(hoursAndMinutes[1])
+
     firebase.database().ref('train/' + name.replace(/ /g, '')).set({
         trainName: name,
         destination: destination,
-        initialTime: time,
+        initialTime: date.toUTCString(),
         frequency: frequency
     })
 }
@@ -22,12 +26,21 @@ function getDatabaseRecords() {
     $('#times').html('<tr><th>Train Name</th><th>Destination</th><th>Frequency</th><th>Next Arrival</th><th>Minutes Away</th></tr>')
     firebase.database().ref('train').once('value').then(function (snapshot) {
         snapshot.forEach(function (entry) {
-            console.log(entry.val())
-            $('#times').append('<tr><td>' + entry.val().trainName + '</td><td>' + entry.val().destination + '</td><td>' + entry.val().frequency + '</td><td>' + entry.val().initialTime + '</td><td>' + 5 + '</td></tr>')
+            var offset = new Date().getTimezoneOffset()
+            var nextArrival = new Date(entry.val().initialTime)
+            
+            console.log(entry.val().initialTime)
+
+            var now = new Date()
+            while(nextArrival < now) {
+                nextArrival = moment(nextArrival).add(entry.val().frequency, 'm').toDate()
+            }
+            var minutesAway = Math.round((nextArrival - now) / 60000)
+
+            $('#times').append('<tr><td>' + entry.val().trainName + '</td><td>' + entry.val().destination + '</td><td>' + entry.val().frequency + '</td><td>' + moment(nextArrival).format('LT') + '</td><td>' + minutesAway + '</td></tr>')
         })
     })
 }
-
 
 $("#submit").click(function () {
     var name = $("#name").val()
@@ -37,17 +50,6 @@ $("#submit").click(function () {
 
     addTrainToDatabase(name, destination, first, min)
     setTimeout(getDatabaseRecords, 200)
-
-
-    console.log(name, destination, first, min)
 })
 
-
-
 getDatabaseRecords()
-
-console.log(moment().format('LT'))
-
-//addTrainToDatabase("MN_Karma_Train", "Miramar", "Now", 25)
-//var hoursAndMinutes = first.split(':')
-//first = new Date(Date.UTC(0, 0, 0, hoursAndMinutes[0], hoursAndMinutes[1]))
